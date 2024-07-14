51fa843bae93f542d56b665a27a12f7009541d8d
python4004 <36403473+python4004@users.noreply.github.com>
2024-07-14 21:29:03 +0300

    Create AtCoder-abc152_a.cpp

---
+++ AtCoder-abc152_a.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int m;
+    cin>>n>>m;
+  if(m==n)
+  {
+      cout<<"Yes"<<endl;
+  }else cout<<"No"<<endl;
+    return 0;
+}
