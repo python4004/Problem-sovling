906ed345fbe175081e64ea75a6ec80d339d72001
python4004 <36403473+python4004@users.noreply.github.com>
2024-07-14 21:27:47 +0300

    Create AtCoder-abc043_a.cpp

---
+++ AtCoder-abc043_a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int k=0;
+    cin>>n;
+    for (int i=0;i<=n;i++)
+    {
+        k=k+i;
+    }
+    cout <<k<<endl;
+
+    return 0;
+}
