d44771cd5df033874df71f36bc6e3827c4a7788a
python4004 <36403473+python4004@users.noreply.github.com>
2024-08-10 02:48:57 +0300

    Update CodeForces - 371C.cpp

--- CodeForces - 371C.cpp
+++ CodeForces - 371C.cpp
@@ -2,54 +2,49 @@
 #include <vector>
 
 using namespace std;
-long long ruble;
-vector<int>must(3,0);
-vector<int>have(3,0);
-vector<int>price(3,0);
 
-bool check(long long mid)
-{
-    long long need=0;
-    for(int i=0;i<3;i++){
-        if(mid*must[i]>have[i]){
-            need+=(mid*must[i]-have[i])*price[i];
-        }
-        if(need>ruble) return false;
+long long ruble;
+vector<long long> must(3, 0);
+vector<long long> have(3, 0);
+vector<long long> price(3, 0);
+
+bool check(long long mid) {
+    long long need = 0;
+    for (int i = 0; i < 3; i++) {
+        long long required = mid * must[i];
+        need += max(0LL, required - have[i]) * price[i];
+        if (need > ruble) return false;  // Early termination if rubles are insufficient
     }
-    return true;
+    return need <= ruble;
 }
 
-int main()
-{
+int main() {
     string ingredient;
-    cin>>ingredient;
-    for(int i=0;i<ingredient.length();i++)
-    {
-        if(ingredient[i]=='B') must[0]++;
-        else if(ingredient[i]=='S') must[1]++;
-        else
-        {
-            must[2]++;
-        }
+    cin >> ingredient;
+    
+    // Use correct type for loop to avoid signed/unsigned comparison warning
+    for (size_t i = 0; i < ingredient.length(); i++) {
+        if (ingredient[i] == 'B') must[0]++;
+        else if (ingredient[i] == 'S') must[1]++;
+        else if (ingredient[i] == 'C') must[2]++;
     }
-    cin>>have[0]>>have[1]>>have[2];
-    cin>>price[0]>>price[1]>>price[2];
-    cin>>ruble;
-    long long low=1, high=1e12,mid=0,ans=low;
-    while(low<=high)
-    {
+    
+    cin >> have[0] >> have[1] >> have[2];
+    cin >> price[0] >> price[1] >> price[2];
+    cin >> ruble;
+
+    long long low = 0, high = 1e13, mid = 0, ans = 0;
+    while (low <= high) {
         mid = low + (high - low) / 2;
-        if(check(mid))
-        {
+        if (check(mid)) {
             ans = mid;
             low = mid + 1;
-        }
-        else
-        {
+        } else {
             high = mid - 1;
         }
     }
-    cout<<ans<<endl;
+    
+    cout << ans << endl;  
 
     return 0;
 }
